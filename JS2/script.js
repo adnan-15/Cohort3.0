91accf9f4ee59adb02c0e88f9a338e8cd7c29169
Adnankhan15929 <khanadnan0339@gmail.com>
2026-07-01 16:21:44 +0530

    types of func

--- JS2/script.js
+++ JS2/script.js
@@ -25,3 +25,34 @@ var eee = () => console.log('helloooloo');
 })()
 
 (()=>console.log('hello'))()
+
+
+function dada(){
+    console.log('I am dada');
+
+    function papa(){
+        console.log('I am papa');
+        
+        function child(){
+            console.log('I am child');
+        }
+        return child
+    }
+    return papa 
+}
+
+
+dada()()()
+
+
+function footpath(w=10){
+    console.log('I am Footpath',w,'feet');
+    
+}
+function mainRoad(wid,cb){
+    console.log('I am Main road',wid,'Feet');
+    cb(wid/10)
+    cb()
+}
+
+mainRoad(80,footpath)
