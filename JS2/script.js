5c17495fd2d4fd1ad4a82fca33d3625f2a3048c7
Adnankhan15929 <khanadnan0339@gmail.com>
2026-07-01 16:22:47 +0530

    func

--- JS2/script.js
+++ JS2/script.js
@@ -1,58 +1,51 @@
 //Function Declaration
 function aaa(a) {
-    console.log('heyyylloo', a);
+  console.log("heyyylloo", a);
 }
 
 // Functions Expressions
 var bbb = function () {
-    console.log('hellooooo');
-}
+  console.log("hellooooo");
+};
 
-// Arrow Functions 
+// Arrow Functions
 var ccc = () => {
-    console.log('hellooollo');
-}
+  console.log("hellooollo");
+};
 
-var ddd = (c, b, a) => console.log('helelele', a);
+var ddd = (c, b, a) => console.log("helelele", a);
 
-var eee = () => console.log('helloooloo');
+var eee = () => console.log("helloooloo");
 
+// IIFE
+// Immidiately Invoked function expression
+(function () {
+  console.log("This is IIFE");
+})()(() => console.log("hello"))();
 
-// IIFE 
-// Immidiately Invoked function expression 
-(function(){
-    console.log('This is IIFE');
-})()
+function dada() {
+  console.log("I am dada");
 
-(()=>console.log('hello'))()
+  function papa() {
+    console.log("I am papa");
 
-
-function dada(){
-    console.log('I am dada');
-
-    function papa(){
-        console.log('I am papa');
-        
-        function child(){
-            console.log('I am child');
-        }
-        return child
+    function child() {
+      console.log("I am child");
     }
-    return papa 
+    return child;
+  }
+  return papa;
 }
 
+dada()()();
 
-dada()()()
-
-
-function footpath(w=10){
-    console.log('I am Footpath',w,'feet');
-    
+function footpath(w = 10) {
+  console.log("I am Footpath", w, "feet");
 }
-function mainRoad(wid,cb){
-    console.log('I am Main road',wid,'Feet');
-    cb(wid/10)
-    cb()
+function mainRoad(wid, cb) {
+  console.log("I am Main road", wid, "Feet");
+  cb(wid / 10);
+  cb();
 }
 
-mainRoad(80,footpath)
+mainRoad(80, footpath);
