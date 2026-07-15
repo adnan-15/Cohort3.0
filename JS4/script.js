beba39d2481c646b9e297eaa1a67abdac60b918f
Adnankhan15929 <khanadnan0339@gmail.com>
2026-07-16 00:09:30 +0530

    phase 4 starting

---
+++ JS4/script.js
@@ -0,0 +1,15 @@
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// console.log(typeof Person);
+
+"use strict";
+
+function show() {
+  console.log(this);
+}
+
+show();
